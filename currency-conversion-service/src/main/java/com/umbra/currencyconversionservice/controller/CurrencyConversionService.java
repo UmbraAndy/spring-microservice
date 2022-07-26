84a5c4509c96a9e4cd608bb5069cb7f9a82c7e74
UmbraKaizen <adeleke.a.babatunde@gmail.com>
2022-07-26 04:50:33 +0100

    Feat: Use feign client for rest call

--- currency-conversion-service/src/main/java/com/umbra/currencyconversionservice/controller/CurrencyConversionService.java
+++ currency-conversion-service/src/main/java/com/umbra/currencyconversionservice/controller/CurrencyConversionService.java
@@ -2,6 +2,8 @@ package com.umbra.currencyconversionservice.controller;
 
 import com.umbra.currencyconversionservice.model.CurrencyConversion;
 import com.umbra.currencyconversionservice.model.CurrencyExchangeResponse;
+import com.umbra.currencyconversionservice.service.CurrencyExchangeServiceProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,9 @@ import java.util.Objects;
 @RestController
 @RequestMapping("currency-conversion-service")
 public class CurrencyConversionService {
+
+    @Autowired
+    private CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
 
     @GetMapping("/from/{fromCurrency}/to/{toCurrency}/quantity/{amount}")
     public CurrencyConversion getConversion(
@@ -42,5 +47,25 @@ public class CurrencyConversionService {
                 exchangeRate.multiply(amount),
                 currencyExchange.environment()
                 );
+    }
+
+    @GetMapping("feign/from/{fromCurrency}/to/{toCurrency}/quantity/{amount}")
+    public CurrencyConversion getConversionUsingFeign(
+            @PathVariable("fromCurrency") String fromCurrency,
+            @PathVariable("toCurrency") String toCurrency,
+            @PathVariable("amount") BigDecimal amount) {
+
+
+        var currencyExchange = currencyExchangeServiceProxy.getExchangeRate(fromCurrency,toCurrency);
+        var exchangeRate = Objects.requireNonNull(currencyExchange).conversionMultiple();
+        return new CurrencyConversion(
+                currencyExchange.id(),
+                currencyExchange.from(),
+                currencyExchange.to(),
+                exchangeRate,
+                amount,
+                exchangeRate.multiply(amount),
+                currencyExchange.environment()
+        );
     }
 }
---
+++ currency-conversion-service/src/main/java/com/umbra/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,16 @@
+package com.umbra.currencyconversionservice.service;
+
+import com.umbra.currencyconversionservice.model.CurrencyExchangeResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+public interface CurrencyExchangeServiceProxy {
+
+    @GetMapping("/currency-exchange-service/from/{from}/to/{to}")
+    public CurrencyExchangeResponse getExchangeRate(
+            @PathVariable String from,
+            @PathVariable String to
+    );
+}
